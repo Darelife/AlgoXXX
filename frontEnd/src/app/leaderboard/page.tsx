826bd88106924947a65ad835267de655c1c2f56d
Prakhar Bhandari <prakharb2k6@gmail.com>
2025-01-25 15:13:16 +0530

    build fix

--- frontEnd/src/app/leaderboard/page.tsx
+++ frontEnd/src/app/leaderboard/page.tsx
@@ -1,36 +1,36 @@
 import React from 'react';
-import UserTable from '../components/UserTable'
+// import UserTable from '../components/UserTable'
 
-interface User {
-  handle: string;
-  name: string;
-  bitsId: string;
-  branch: string;
-  peakRating: number;
-  currentRating: number;
-}
+// interface User {
+//   handle: string;
+//   name: string;
+//   bitsId: string;
+//   branch: string;
+//   peakRating: number;
+//   currentRating: number;
+// }
 
 export default function TablePage() {
-    const [users, setUsers] = React.useState<User[]>([]);
+    // const [users, setUsers] = React.useState<User[]>([]);
 
-    React.useEffect(() => {
-      async function fetchUsers() {
-        try {
-          const response = await fetch('/api/users');
-          const data = await response.json();
-          setUsers(data);
-        } catch (error) {
-          console.error('Error fetching users:', error);
-        }
-      }
+    // React.useEffect(() => {
+    //   async function fetchUsers() {
+    //     try {
+    //       const response = await fetch('/api/users');
+    //       const data = await response.json();
+    //       setUsers(data);
+    //     } catch (error) {
+    //       console.error('Error fetching users:', error);
+    //     }
+    //   }
 
-      fetchUsers();
-    }, []);
+    //   fetchUsers();
+    // }, []);
   return (
     <>
       <div>
         <h1>User Leaderboard</h1>
-        <UserTable users={users} />
+        {/* <UserTable users={users} /> */}
       </div>
     </>
   )
