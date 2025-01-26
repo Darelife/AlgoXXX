af5613c17419e740ed0d9a9136bd5f4eaffce1d8
Prakhar Bhandari <prakharb2k6@gmail.com>
2025-01-26 16:45:23 +0530

    Added analytics

--- frontEnd/src/app/layout.tsx
+++ frontEnd/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,10 +27,12 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" />
+        <script defer src="https://cloud.umami.is/script.js" data-website-id="68577f98-65bc-460a-a02a-6675a211522e"></script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased vsc-initialized`}
       >
+        <Analytics />
         {children}
       </body>
     </html>
