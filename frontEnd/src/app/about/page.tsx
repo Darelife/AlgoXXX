784e384f6360ffc7d6e04e30ae510e2be1b8c165
Prakhar Bhandari <prakharb2k6@gmail.com>
2025-02-05 23:16:39 +0530

    build fix

--- frontEnd/src/app/about/page.tsx
+++ frontEnd/src/app/about/page.tsx
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from "react";
 import NavBar from "../components/navBar";
 import Image from "next/image";
-import TextIntoView from "../components/typeIntoView";
+// import TextIntoView from "../components/typeIntoView";
 
 export default function Home() {
   const [theme, setTheme] = useState("light");
