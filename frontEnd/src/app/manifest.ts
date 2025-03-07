b44d2fb7acb2c966c87dd16e57904fb2e91ef61c
Prakhar Bhandari <prakharb2k6@gmail.com>
2025-03-07 23:10:26 +0530

    updated the the image url for the manifest

--- frontEnd/src/app/manifest.ts
+++ frontEnd/src/app/manifest.ts
@@ -11,7 +11,7 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#000000',
     icons: [
       {
-        src: '/algoLightXLogo.png',
+        src: '/algoDarkLogoSmall.png',
         sizes: '192x192',
         type: 'image/png',
       }
